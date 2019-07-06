a73341e0b9df31d325bf4e349504a1d328f192b1
munasing <madhusankaashan41@gmail.com>
2019-07-06 23:41:06 +0530

    Final  commit

---
+++ src/main/java/pizzaloop/MainController.java
@@ -0,0 +1,65 @@
+package pizzaloop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController {
+    @Autowired
+    private PizzaRepository pizzaRepository;
+    private static final String SUCCESS= "Saved";
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<PizzaDetails> getPizzaDetails() {
+        return pizzaRepository.findAll();
+    }
+
+
+    @GetMapping(path="/findByPizzaId")
+    public @ResponseBody List<PizzaDetails> getPizzaById(@RequestParam Integer id) {
+        return pizzaRepository.findByPizzaId(id);
+    }
+
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewPizza(@RequestParam String name, @RequestParam String description, @RequestParam Double price) {
+        PizzaDetails pizzaDetails = new PizzaDetails();
+        pizzaDetails.setName(name);
+        pizzaDetails.setDescription(description);
+        pizzaDetails.setPrice(price);
+        pizzaRepository.save(pizzaDetails);
+        return SUCCESS;
+    }
+
+
+    @GetMapping(path="/deleteByPizzaId")
+    public @ResponseBody List<PizzaDetails> deletePizzaById(@RequestParam Integer id) {
+        return pizzaRepository.deleteByPizzaId(id);
+    }
+
+
+    @GetMapping(path="/update")
+    public @ResponseBody List<PizzaDetails> updatePizzaDetails(@RequestParam Integer id, @RequestParam String name, @RequestParam String description, @RequestParam Double price) {
+
+        List<PizzaDetails> pizzaDetailsList = pizzaRepository.findByPizzaId(id);
+        if(!pizzaDetailsList.isEmpty()) {
+
+            for(PizzaDetails pizzaDetails: pizzaDetailsList) {
+
+                pizzaDetails.setName(name);
+                pizzaDetails.setDescription(description);
+                pizzaDetails.setPrice(price);
+
+                pizzaRepository.save(pizzaDetails);
+            }
+        }
+        return pizzaRepository.findByPizzaId(id);
+    }
+}
